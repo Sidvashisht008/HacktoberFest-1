91379c061ccc7ff1c9f14175bdc9eedd6dbceed4
Siddharth Vashisht <svashisht008@gmail.com>
2021-10-26 21:57:22 +0530

    Create stone.java

---
+++ stone.java
@@ -0,0 +1,29 @@
+package code;
+
+public class stone {
+	int weightOfStone;
+	int densityOfStone;
+	int angleOfthrow;
+	public stone(int w,int d, int a) {
+		this.weightOfStone = w;
+		this.densityOfStone = d;
+		this.angleOfthrow = a;
+	}
+	
+	public int heightOfSplash() {
+		
+	}
+	
+	public int HowFarstoneTravels() {
+		
+	}
+	public int HowManyTimesItTouchesWater() {
+		
+	}
+	class water{
+		int density
+		public int radiusOfRipplesFormed() {
+			
+		}
+	}
+}
